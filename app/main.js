f072e9a80ec61beb9704df3a340cc82579aad026
nqx2007 <nqx2007@users.noreply.github.com>
2017-08-24 17:24:34 +0800

    Create main.js

---
+++ app/main.js
@@ -0,0 +1,8 @@
+ import React from 'react';
+import ReactDom from 'react-dom';
+import Component from './components/Component.jsx';
+import './public/styles/index.css'
+ReactDom.render(
+    <Component />,
+    document.getElementById('content')
+)
