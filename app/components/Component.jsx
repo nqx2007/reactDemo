f5359f33976b245974c1f033ef40e1120646953b
nqx2007 <nqx2007@users.noreply.github.com>
2017-08-24 17:26:04 +0800

    Create Component.jsx

---
+++ app/components/Component.jsx
@@ -0,0 +1,13 @@
+class Component extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title">我的第一个React+Webpack项目</h1>
+                <p className="text">hahahaahaha</p>
+            </div>
+        )
+    }
+}
+
+//导出组件
+export default Component;
